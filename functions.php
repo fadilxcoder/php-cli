8a3b922b78ecbb832b9a8805256e68665ec47ee0
Fadil <rosunmungurudm@gmail.com>
2020-02-03 20:42:25 +0400

    Source code

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+    require_once('db-connect.php');
+
+    # Errors
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    # Database Manipulation
+    function converter($query)
+    {
+        $arr = array();
+        while( $data = $query->fetch_assoc()):
+            $arr[] = $data;
+        endwhile;
+        return $arr;
+    }
+
+    function selectAll()
+    {
+        global $connection;
+        $sql    = "SELECT * FROM vip_list ORDER BY id ASC";
+        $query  = $connection->query($sql);
+        $result = converter($query);
+        return $result;
+    }
+?>
---
+++ libs/mailer.php
@@ -0,0 +1,43 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    function massMailing()
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug 	= SMTP::DEBUG_LOWLEVEL;                      
+            $mail->isSMTP();                                            
+            $mail->Host       	= 'smtp.mailtrap.io';                
+            $mail->SMTPAuth   	= true;                               
+            $mail->Username   	= '';                 
+            $mail->Password   	= '';                           
+            $mail->SMTPSecure 	= PHPMailer::ENCRYPTION_STARTTLS;     
+            $mail->Port       	= 2525;                                
+        
+            
+
+            $mail->isHTML(true); 
+        
+            foreach (selectAll() as $data) {
+                //Recipients
+                $mail->setFrom('no-reply@fadilxcoder.dev', 'FX');
+                $mail->addAddress($data['email'], $data['name'].' '.$data['surname']); 
+                $mail->addReplyTo('fadil@xcoder.dev', 'FRACTAL X - Techno For Acid');
+                $mail->Subject = 'Techno For Acid Rave VIP';
+                $mail->Body    = '<p>Hi,<br><br> Calling for Ravers @ <b>Techno for Acid Rave VIP</b> Party.<br> More information will be provided soon !<br><br><small>FRACTAL X</small></p>';
+                $mail->AltBody = 'Calling for Ravers at Techno for Acid Rave VIP Party. More information will be provided soon ! FRACTAL X';
+            
+                $mail->send();
+                echo 'Message has been sent';
+            }
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+
+?>
